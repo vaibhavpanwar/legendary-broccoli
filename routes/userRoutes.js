3beee9fdca3aa2e6b93063397ec31fc7298d90f4
vaibhav <vaibhavpanwar1402@gmail.com>
2021-05-25 00:39:00 +0530

    node js authentication boiler plate

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.get("/profile", protect, getUserProfile);
+export default router;
